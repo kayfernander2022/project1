03286d2c83bdaf521e9b17399bffe958d721ca88
kayfernander2022 <kayfernander@hotmail.com>
2022-10-24 12:28:43 -0400

    display data

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const BASE_URL = "https://api.giphy.com/v1"
 const API_KEY = "grszT8PPh8t8tXcWMfVO1FkF0nf4FGBP"
 const GIF_SEARCH_URL = `${BASE_URL}/gifs/search?api_key=${API_KEY}&limit=25&offset=0&rating=g&lang=en`;
 
-function searchGif(searchTerm){
+//return the data
+function searchGif(searchTerm) { 
   $.ajax({url: `${GIF_SEARCH_URL}&q=${searchTerm}`})
   .then((response) =>{
-    return response.data;
+    
+    displayResults(response.data);
   })
   .catch((error) =>{
     console.log("There was a problem " + error);
-  })
+  });
 }
 
+// loop and display the data i need
+function displayResults(results){
+  const gifItems = results.map((item) =>{
+  const image = item.images.original
+  //const author = item.user
+  return{
+    //author: author.display_name, //error cant see author?
+    title: item.title,
+    image: {
+      url: image.url,
+      height: image.height,
+      width: image.width
+    }
+  }
 
-searchGif("ballons");
+});
+console.log(gifItems)
+}
+
+searchGif("balloons")
